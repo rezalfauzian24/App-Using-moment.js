62f685d4e9abd14a8ccddc0bc5c7014e36d95479
rezalfauzian24 <rezalofficial732@gmail.com>
2026-07-02 09:57:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "name": "app.py",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
